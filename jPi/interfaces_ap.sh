023a3e9bd2a24df5fcb0201d8d14769ce3168352
PyJaejin <jaen6563@gmail.com>
2018-11-27 17:29:44 +0000

    change

--- jPi/interfaces_ap.sh
+++ jPi/interfaces_ap.sh
@@ -1,1 +1,2 @@
 sudo cp /etc/network/interfaces.apmode /etc/network/interfaces
+sudo cp ~/jPi/rc.local.ap /etc/rc.local
--- jPi/interfaces_wifi.sh
+++ jPi/interfaces_wifi.sh
@@ -1,1 +1,2 @@
 sudo cp /etc/network/interfaces.wifimode /etc/network/interfaces
+sudo cp ~/jPi/rc.local.wifi /etc/rc.local
